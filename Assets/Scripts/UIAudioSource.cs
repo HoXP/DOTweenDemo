cebdbf72b37a3aad978743f9e60d214706894e3d
xiangping.hu <xiangping.hu@17zuoye.com>
2019-01-14 14:51:00 +0800

    DOPitch

--- Assets/Scripts/UIAudioSource.cs
+++ Assets/Scripts/UIAudioSource.cs
@@ -5,17 +5,39 @@ using UnityEngine.UI;
 
 public class UIAudioSource : UIBasePanel
 {
+    public enum DoAudioSource
+    {
+        DOFade = 0,
+        DOPitch
+    }
+    private DoAudioSource enmDoAudioSource = DoAudioSource.DOFade;
+
+    private Dropdown _dpdFunc = null;
+    private Slider _sldTo = null;
+    private InputField _iptDuration = null;
+
     private AudioSource _ads = null;
-    private Scrollbar _scbVolume = null;
-    private Text _txtVolume = null;
+    private Scrollbar _scb = null;
+    private Text _txtScb = null;
     private Button _btnDo = null;
 
     protected override void Init()
     {
         base.Init();
+        
+        _dpdFunc = transform.Find("Adapter/dpdFunc").GetComponent<Dropdown>();
+        DoAudioSource[] values = Enum.GetValues(typeof(DoAudioSource)) as DoAudioSource[];
+        for (int i = 0; i < values.Length; i++)
+        {
+            _dpdFunc.options.Add(new Dropdown.OptionData(values.GetValue(i).ToString()));
+        }
+        _dpdFunc.onValueChanged.AddListener(OnDpdFuncSelect);
+        _sldTo = transform.Find("Adapter/sldTo").GetComponent<Slider>();
+        _iptDuration = transform.Find("Adapter/iptDuration").GetComponent<InputField>();
+
         _ads = transform.Find("Adapter/ads").GetComponent<AudioSource>();
-        _scbVolume = transform.Find("Adapter/scbVolume").GetComponent<Scrollbar>();
-        _txtVolume = _scbVolume.transform.Find("Sliding Area/Handle/txtVolume").GetComponent<Text>();
+        _scb = transform.Find("Adapter/scb").GetComponent<Scrollbar>();
+        _txtScb = _scb.transform.Find("SlidingArea/Handle/txtVolume").GetComponent<Text>();
         _btnDo = transform.Find("Adapter/btnDo").GetComponent<Button>();
         _btnDo.onClick.AddListener(OnClickBtnDo);
     }
@@ -23,27 +45,51 @@ public class UIAudioSource : UIBasePanel
     private void Start()
     {
         _ads.Play();
+        _dpdFunc.value = 0;
+        _dpdFunc.RefreshShownValue();
+        _iptDuration.text = "1";
+        UpdateFloat(1);
     }
-    
-    void Update()
-    {
 
+    private void OnDpdFuncSelect(int arg0)
+    {
+        enmDoAudioSource = (DoAudioSource)arg0;
     }
 
     protected override void OnClickBtnHome()
     {
         base.OnClickBtnHome();
-        UpdateVolume(1);
+        UpdateFloat(1);
     }
 
     private void OnClickBtnDo()
     {
-        _ads.DOFade(.5f, 1).OnUpdate(()=> { UpdateVolume(_ads.volume); });
+        float endValue = _sldTo.value;
+        float duration = 0;
+        float.TryParse(_iptDuration.text, out duration);
+        switch (enmDoAudioSource)
+        {
+            case DoAudioSource.DOFade:
+                _ads.DOFade(endValue, duration).OnUpdate(() => { UpdateFloat(_ads.volume); });
+                break;
+            case DoAudioSource.DOPitch:
+                _ads.DOPitch(endValue, duration).OnUpdate(() => { UpdateFloat(_ads.pitch); });
+                break;
+        }
     }
-
-    private void UpdateVolume(float vol)
+    
+    private void UpdateFloat(float val)
     {
-        _ads.volume = vol;
-        _scbVolume.value = vol;
+        switch (enmDoAudioSource)
+        {
+            case DoAudioSource.DOFade:
+                _ads.volume = val;
+                break;
+            case DoAudioSource.DOPitch:
+                _ads.pitch = val;
+                break;
+        }
+        _scb.value = val;
+        _txtScb.text = val.ToString();
     }
 }
