50882b316303ba2cc3b4605c3d88816dcd8f2dd9
xiangping.hu <xiangping.hu@17zuoye.com>
2019-01-14 16:32:11 +0800

    pitch不是[0,1]范围

--- Assets/Scripts/UIAudioSource.cs
+++ Assets/Scripts/UIAudioSource.cs
@@ -14,11 +14,11 @@ public class UIAudioSource : UIBasePanel
 
     private Dropdown _dpdFunc = null;
     private Slider _sldTo = null;
+    private InputField _iptToPitch = null;
     private InputField _iptDuration = null;
 
     private AudioSource _ads = null;
-    private Scrollbar _scb = null;
-    private Text _txtScb = null;
+    private Text _txtVal = null;
     private Button _btnDo = null;
 
     protected override void Init()
@@ -33,11 +33,11 @@ public class UIAudioSource : UIBasePanel
         }
         _dpdFunc.onValueChanged.AddListener(OnDpdFuncSelect);
         _sldTo = transform.Find("Adapter/sldTo").GetComponent<Slider>();
+        _iptToPitch = transform.Find("Adapter/iptToPitch").GetComponent<InputField>();
         _iptDuration = transform.Find("Adapter/iptDuration").GetComponent<InputField>();
 
         _ads = transform.Find("Adapter/ads").GetComponent<AudioSource>();
-        _scb = transform.Find("Adapter/scb").GetComponent<Scrollbar>();
-        _txtScb = _scb.transform.Find("SlidingArea/Handle/txtVolume").GetComponent<Text>();
+        _txtVal = transform.Find("Adapter/txtVal").GetComponent<Text>();
         _btnDo = transform.Find("Adapter/btnDo").GetComponent<Button>();
         _btnDo.onClick.AddListener(OnClickBtnDo);
     }
@@ -49,30 +49,58 @@ public class UIAudioSource : UIBasePanel
         _dpdFunc.RefreshShownValue();
         _iptDuration.text = "1";
         UpdateFloat(1);
+        ShowHideConfig();
+    }
+
+    private void ShowHideConfig()
+    {
+        switch (enmDoAudioSource)
+        {
+            case DoAudioSource.DOFade:
+                _sldTo.gameObject.SetActive(true);
+                _iptToPitch.gameObject.SetActive(false);
+                break;
+            case DoAudioSource.DOPitch:
+                _sldTo.gameObject.SetActive(false);
+                _iptToPitch.gameObject.SetActive(true);
+                break;
+        }
     }
 
     private void OnDpdFuncSelect(int arg0)
     {
         enmDoAudioSource = (DoAudioSource)arg0;
+        ShowHideConfig();
     }
 
     protected override void OnClickBtnHome()
     {
         base.OnClickBtnHome();
-        UpdateFloat(1);
+        _ads.volume = 1;
+        _ads.pitch = 1;
+        _txtVal.text = "1";
     }
 
     private void OnClickBtnDo()
     {
-        float endValue = _sldTo.value;
+        float endValue = 0;
         float duration = 0;
         float.TryParse(_iptDuration.text, out duration);
         switch (enmDoAudioSource)
         {
             case DoAudioSource.DOFade:
+                endValue = _sldTo.value;
+                /*
+                 * duration秒内将一个AudioSource的音量Tween到endValue
+                 * endValue∈[0,1]
+                 */
                 _ads.DOFade(endValue, duration).OnUpdate(() => { UpdateFloat(_ads.volume); });
                 break;
             case DoAudioSource.DOPitch:
+                float.TryParse(_iptToPitch.text, out endValue);
+                /*
+                 * duration秒内将一个AudioSource的pitch（音高）Tween到endValue
+                 */
                 _ads.DOPitch(endValue, duration).OnUpdate(() => { UpdateFloat(_ads.pitch); });
                 break;
         }
@@ -89,7 +117,6 @@ public class UIAudioSource : UIBasePanel
                 _ads.pitch = val;
                 break;
         }
-        _scb.value = val;
-        _txtScb.text = val.ToString();
+        _txtVal.text = val.ToString();
     }
 }
